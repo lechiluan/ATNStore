8567d59ffaf7515f52d505fd977c1b8e6d6e351e
Lê Chí Luân <chiluan6601@gmail.com>
2022-07-05 17:09:57 +0700

    update

--- Index/Header.php
+++ Index/Header.php
@@ -3,7 +3,7 @@
 
 <head>
     <title>ATN Store</title>
-    <link rel="shortcut icon" href="/images/logoATN.png" />
+    <link rel="shortcut icon" href="/images/logomini.png" />
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="style.css">
