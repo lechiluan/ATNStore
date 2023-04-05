bdf4dd74b553e731e65d07003ca73c15cd635e56
Lê Chí Luân <chiluan6601@gmail.com>
2023-04-05 10:57:49 +0700

    update

--- Connection.php
+++ Connection.php
@@ -1,5 +1,7 @@
 <?php
-	$conn = pg_connect("postgres://fsttwdojipdhgw:0b468fb310acc2bfbe392e6dcc7068fc2c90dddc2c755b42a780d9241cd34a3c@ec2-54-167-201-170.compute-1.amazonaws.com:5432/d47ede5l9i24s8");
+	$connectionString = ("host=localhost port=5432 user=postgres password=19012001 dbname=atnstore");
+	$conn = pg_connect($connectionString) or die("Could not connect to database");
+
 	if(!$conn)
 	{
 		die("Could not connect to database");
