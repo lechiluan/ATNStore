bf3b0546d00927a5a59a906be19ff3b7606bae70
Lê Chí Luân <chiluan6601@gmail.com>
2023-05-04 09:51:37 +0700

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -1,5 +1,5 @@
 <?php
-	$connectionString = ("host=localhost port=5432 user=postgres password=19012001 dbname=atnstore");
+	$connectionString = ("postgresql://uprqn1rwydlw9kvfdult:ilAzQNpViBzElprlNi12m98qYXBnSh@bichfrlbt6aeeyglyhng-postgresql.services.clever-cloud.com:5432/bichfrlbt6aeeyglyhng");
 	$conn = pg_connect($connectionString) or die("Could not connect to database");
 
 	if(!$conn)
